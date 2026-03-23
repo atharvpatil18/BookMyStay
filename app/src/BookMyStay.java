0f135badf7eb44d6466cd5cc5bbaafdd77e8a693
atharvpatil18 <atharvpatil18@users.noreply.github.com>
2026-03-23 15:02:05 +0530

    UC1: Welcome page implementation

---
+++ app/src/BookMyStay.java
@@ -0,0 +1,9 @@
+public class BookMyStay {
+    public static void main(String[] args) {
+
+        //USE CASE 1
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+        System.out.println("Application: Book My Stay App v1.0");
+    }
+}
